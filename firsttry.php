919843368b875ce302daaa41eab326316a1a3375
Dmitry <GreyDiamond@rambler.ru>
2012-06-21 18:07:51 +0600

    на что хватило

--- firsttry.php
+++ firsttry.php
@@ -1,18 +1,32 @@
-<?php
-if ($_POST) {
-    echo '<pre>';
-    echo htmlspecialchars(print_r($_POST, true));
-    echo '</pre>';
-}
+<HTML>
+<TITLE> ХЗ МАГАЗИН ТИПА</TITLE>
+<BASE href="http://localhost/">
+<HEAD>
+
+<p><h1 align="left">здесь будут располагаться элементы заголовка документа\|/<h1><p> 
+</HEAD>
+<p><p>
+<BODY bgcolor="black" text="#FF0000" link="blue" alink=
+"red" vlink="purple">
+<p><h2 align="right">\|/здесь будут располагаться элементы тела документа<h2><p>
+<?php//пока непойму что сделать пхп скриптом (((
 ?>
-<form action="" method="post">
-    ИМЯ:  <input type="text" name="personal[name]" /><br />
-    МЫЛО: <input type="text" name="personal[email]" /><br />
-    ПИВО: <br />
-    <select multiple name="beer[]">
-        <option value="warthog">Warthog</option>
-        <option value="guinness">Guinness</option>
-        <option value="stuttgarter">Stuttgarter Schwabenbrau</option>
-    </select><br />
-    <input type="submit" value="тоже сабмит!" />
-</form> 
+<div align="center">
+ <form name="simple_product_form" method="post">
+    <p><B>типо товар</B><p>  
+    <p><a href="images/full_image.jpeg" target="blank"><IMG src="images/small_image.jpeg" name="name" /></a><p>
+    <input type="submit" value="добавить"/>  
+ </form>
+ <form name="simple_product_form1" method="post">
+    <p><B>типо товар1</B><p>   
+    <p><IMG src="" name="name" /><p>
+    <input type="submit" value="добавить"/>  
+ </form>
+ <form name="simple_product_form2" method="post">
+    <p><B>типо товар2</B><p>  
+    <p><IMG src="" name="name" /><p>
+    <input type="submit" value="добавить"/>  
+ </form>
+</div>
+</BODY>
+</HTML>
