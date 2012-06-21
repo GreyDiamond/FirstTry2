555134bee2c89f31ba89b5b7f6993b8296081571
Dmitry <GreyDiamond@rambler.ru>
2012-06-21 10:16:09 +0600

    задолбал

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+if ($_POST) {
+    echo '<pre>';
+    echo htmlspecialchars(print_r($_POST, true));
+    echo '</pre>';
+}
+?>
+<form action="" method="post">
+    Имя:  <input type="text" name="personal[name]" /><br />
+    Email: <input type="text" name="personal[email]" /><br />
+    Пиво: <br />
+    <select multiple name="beer[]">
+        <option value="warthog">Warthog</option>
+        <option value="guinness">Guinness</option>
+        <option value="stuttgarter">Stuttgarter Schwabenbrau</option>
+    </select><br />
+    <input type="submit" value="Отправь меня!" />
+</form> 
